455e7da320ace0e82a8234a88656daa6a7abde19
David de Knegt <33622110+DavisKarper@users.noreply.github.com>
2024-01-19 10:14:25 +0100

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,8 @@
 <?php
-$host       = "https://sql.hosted.hro.nl/";
+$host       = "localhost";
 $database   = "prj_2023_2024_ressys_t9";
-$user       = "1075509";
-$password   = "zaedoodi";
+$user       = "prj_2023_2024_ressys_t9";
+$password   = "niroofoo";
 
 $db = mysqli_connect($host, $user, $password, $database)
     or die("Error: " . mysqli_connect_error());;
